b205371743e67b52b46fc56fea7d56679c794612
Vinícius <vinicius.ferreira@ufpe.br>
2025-08-08 19:24:55 -0300

    F1352: primeiros gráficos

--- main.py
+++ main.py
@@ -1,10 +1,33 @@
 import streamlit as st
 import pandas as pd
+import altair as alt
 
-# Read the CSV with new headers
 df = pd.read_csv(
   "leei.csv",
   header=None,
   names=['#', 'RELATÓRIO', 'NOME', 'MUNICÍPIO', 'UF', 'TURMA', 'CONTRATO', 'OFÍCIO', 'DATA'],
 )
-st.dataframe(df, hide_index=True)
+
+st.dataframe(
+  df,
+  hide_index=True
+)
+
+# Gráfico com o Streamlit
+st.bar_chart(df['UF'].value_counts())
+
+# Gráfico com o Altair
+frequency_series = df['UF'].value_counts()
+
+frequency_df = frequency_series.reset_index()
+frequency_df.columns = ['Category', 'Count']
+
+chart = alt.Chart(frequency_df).mark_bar().encode(
+    y=alt.Y('Category:N', title='Category'),  # 'N' para dados nominais/qualitativos
+    x=alt.X('Count:Q', title='Frequency'),    # 'Q' para dados quantitativos
+    tooltip=['Category', 'Count']
+).properties(
+    title='Número de solicitações de pagamento por UF'
+)
+
+st.altair_chart(chart, use_container_width=True)
